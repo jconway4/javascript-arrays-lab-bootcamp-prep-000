5f7e75f0b68f0f4b3c5833dfd8be5dcba5e0bb99
jconway4 <jconwa4@illinois.edu>
2018-03-14 18:46:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-const app = "I don't do much."
+const app = "I don't do much.";
 
 var destructivelyAppendKitten = function(name) {
   kittens.push(name);
-}
+};
 
 var destructivelyPrependKitten = function(name) {
   kittens.unshift(name);
-}
+};
 
 var destructivelyRemoveLastKitten = function(name) {
   kittens.pop();
-}
+};
 
 var destructivelyRemoveFirstKitten = function(name) {
   kittens.shift();
-}
+};
 
